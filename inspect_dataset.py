9fd30913d57afd5bd63a4309f0d6e7c9ebda4293
Merve Vural <95167409+mervehmv@users.noreply.github.com>
2026-03-23 01:02:37 +0300

    Delete inspect_dataset.py

--- inspect_dataset.py
+++
@@ -1,6 +0,0 @@
-import datasets
-import json
-
-ds = datasets.load_dataset('parquet', data_files='/arf/scratch/hvural/DeepEyes/caption_dataset_rl_v6_env_clean/train-00000-of-00008.parquet')['train']
-with open('/arf/scratch/hvural/DeepEyes/debug_ds.json', 'w') as f:
-    json.dump(ds[0], f, indent=4, default=str)
